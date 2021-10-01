f81a59e81f11e52a7f36d730d4db173da226389e
jesusMelian <usr2685@salesianos-lacuesta.net>
2021-10-01 11:24:33 +0100

    Features. Creacion de clases principales y estructura de herencias

---
+++ src/com/JesusMelian/zoo/Management/Grab.java
@@ -0,0 +1,11 @@
+package com.JesusMelian.zoo.Management;
+
+import com.JesusMelian.zoo.Animals.Animals;
+
+import java.util.ArrayList;
+
+public class Grab {
+    public Grab(Animals animal) {
+        
+    }
+}
---
+++ src/com/JesusMelian/zoo/Management/Main.java
@@ -0,0 +1,13 @@
+package com.JesusMelian.zoo.Management;
+
+import com.JesusMelian.zoo.Animals.Animals;
+import com.JesusMelian.zoo.types.Birds;
+import com.JesusMelian.zoo.types.Mamals;
+import com.JesusMelian.zoo.types.Reptile;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
